5afe4391a446f109175189d49646a5438572598b
zinfotech1996-ui <zinfotech1996@gmail.com>
2026-01-18 11:29:59 +0800

    1

--- vite.config.ts
+++ vite.config.ts
@@ -18,7 +18,6 @@
 //     },
 //   },
 // }));
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
@@ -30,7 +29,7 @@ export default defineConfig({
   },
   plugins: [react()],
   // Use relative paths for GitHub Pages
-  base: './',
+  base: '/omni-gratum/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
