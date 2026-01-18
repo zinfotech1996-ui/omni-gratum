14712f8f18ca68f5ca15c54d725f22142c3b452c
zinfotech1996-ui <zinfotech1996@gmail.com>
2026-01-18 10:25:11 +0800

    1

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig(({ mode }) => ({
   plugins: [
     react()
   ].filter(Boolean),
-  base:"omni-gratum",
+  base:"/omni-gratum",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
